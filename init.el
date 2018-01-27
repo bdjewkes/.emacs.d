51214b1b226b91b0fc03b094dee0c288bf9c4b39
Brian Jewkes <bdjewkes@gmail.com>
2018-01-27 14:13:28 -0800

    elfeed config

--- init.el
+++ init.el
@@ -12,24 +12,4 @@
   (package-refresh-contents))
 
 
-(org-babel-load-file "./configuration.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(custom-safe-themes
-   (quote
-    ("25c06a000382b6239999582dfa2b81cc0649f3897b394a75ad5a670329600b45" "9a155066ec746201156bb39f7518c1828a73d67742e11271e4f24b7b178c4710" "43c1a8090ed19ab3c0b1490ce412f78f157d69a29828aa977dae941b994b4147" "36282815a2eaab9ba67d7653cf23b1a4e230e4907c7f110eebf3cdf1445d8370" default)))
- '(hl-sexp-background-color "#efebe9")
- '(package-selected-packages
-   (quote
-    (helm-projectile markdown-mode+ markdown-mode cargo rust-mode zenburn-theme racket-mode leuven-theme helm geiser evil-tabs evil-paredit evil-org evil-magit evil-leader distinguished-theme darktooth-theme color-theme-solarized color-theme-sanityinc-tomorrow base16-theme ace-jump-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(org-babel-load-file "~/.emacs.d/configuration.org")
