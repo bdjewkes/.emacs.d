60fb21b0f5074e7e7eb0e51f0e7f49b3ed49c41c
Brian Jewkes <bdjewkes@gmail.com>
2018-09-17 12:49:50 -0700

    gtd implementation, etc

--- init.el
+++ init.el
@@ -7,20 +7,27 @@
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
+
 ;; Download latest package info if needed.
-(when (not package-archive-contents)
-  (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-
+(eval-when-compile
+  (require 'use-package))
 (org-babel-load-file "~/.emacs.d/configuration.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("a566448baba25f48e1833d86807b77876a899fc0c3d33394094cf267c970749f" "8891c81848a6cf203c7ac816436ea1a859c34038c39e3cf9f48292d8b1c86528" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" "9d9fda57c476672acd8c6efeb9dc801abea906634575ad2c7688d055878e69d6" "c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" "d5b121d69e48e0f2a84c8e4580f0ba230423391a78fcb4001ccb35d02494d79e" "e11569fd7e31321a33358ee4b232c2d3cf05caccd90f896e1df6cab228191109" default)))
+ '(dumb-jump-mode t)
  '(package-selected-packages
    (quote
-    (dumb-jump neotree zenburn-theme racket-mode ob-rust markdown-mode leuven-theme jinja2-mode helm-projectile geiser flycheck evil-tabs evil-paredit evil-org evil-magit evil-leader elfeed distinguished-theme darktooth-theme color-theme-solarized color-theme-sanityinc-tomorrow cargo base16-theme auto-complete ace-jump-mode))))
+    (restclient web-mode tide all-the-icons-dired use-package dired-sidebar spaceline-all-the-icons spaceline fsharp-mode typescript-mode ag doom-themes doom jedi elpy dumb-jump neotree zenburn-theme racket-mode ob-rust markdown-mode leuven-theme jinja2-mode helm-projectile geiser flycheck evil-tabs evil-paredit evil-org evil-magit evil-leader elfeed distinguished-theme darktooth-theme color-theme-solarized color-theme-sanityinc-tomorrow cargo base16-theme auto-complete ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
